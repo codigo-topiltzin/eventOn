b5ee4179f2b024326c9c4a4d029e003cdef3484a
erick14911 <erickdlr1945@gmail.com>
2025-03-08 17:38:54 -0600

    perf:change db_pilares.sql

--- db_pilares.sql
+++ db_pilares.sql
@@ -1,6 +1,19 @@
+
+/* 
+CREATE DATABASE db_codigo_topiltzin CHARACTER SET utf8mb4;
+CREATE USER IF NOT EXISTS 'account'@%
+GRANT ALL PRIVILEGES
+ON `db\_session`.* 
+TO 'ErickDeLaRosa'@'%' INDENTIFIED BY 'password' WITH GRANT OPTION;
+*/
+USE db_codigo_topiltzin;
+
 START TRANSACTION;
-CREATE TABLE IF NOT EXISTS tableA(
-        GENERATED ALWAYS AS (),
+CREATE TABLE IF NOT EXISTS usuario(
+        id INT UNSIGNED AUTO_INCREMENT,
+        name VARCHAR(100) NOT NULL,
+        apellidos VARCHAR(100) NOT NULL,
+        full_name GENERATED ALWAYS AS (),
         CONSTRAINT pk_tableA PRIMARY KEY(id)
         )ENGINE=InnoDb
         DEFAULT CHARSET = utf8mb4;
