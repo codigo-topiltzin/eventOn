f26871d5e1989cd262a79f23a16d9fda20483e9c
erick14911 <erickdlr1945@gmail.com>
2025-03-08 18:47:46 -0600

    perf:change dv_pilares.sql

--- db_pilares.sql
+++ db_pilares.sql
@@ -9,14 +9,37 @@ TO 'ErickDeLaRosa'@'%' INDENTIFIED BY 'password' WITH GRANT OPTION;
 USE db_codigo_topiltzin;
 
 START TRANSACTION;
-CREATE TABLE IF NOT EXISTS usuario(
+CREATE TABLE IF NOT EXISTS users(
+        id INT UNSIGNED AUTO_INCREMENT,
+        first_name VARCHAR(100) NOT NULL,
+        last_name VARCHAR(100) NOT NULL,
+        full_name GENERATED ALWAYS AS (CONCAT(first_name, " ", last_name)),
+        folio VARCHAR(12) NOT NULL,
+        age INT UNSIGNED,
+        sex VARCHAR(20),
+        phone VARCHAR(12),
+        email VARCHAR(50),
+        CONSTRAINT pk_users PRIMARY KEY(id)
+)ENGINE=InnoDb;
+CREATE TABLE IF NOT EXISTS events(
         id INT UNSIGNED AUTO_INCREMENT,
         name VARCHAR(100) NOT NULL,
-        apellidos VARCHAR(100) NOT NULL,
-        full_name GENERATED ALWAYS AS (),
-        CONSTRAINT pk_tableA PRIMARY KEY(id)
-        )ENGINE=InnoDb
-        DEFAULT CHARSET = utf8mb4;
+        date_e DATE NOT NULL,
+        start_e TIME NOT NULL,
+        finish_e TIME NOT NULL,
+        CONSTRAINT pk_events PRIMARY KEY(id)
+)ENGINE=InnoDb;
+CREATE TABLE IF NOT EXISTS assistant(
+        id INT UNSIGNED AUTO_INCREMENT,
+        rol VARCHAR(100) NOT NULL,
+        user_pilares INT UNSIGNED,
+        event INT UNSIGNED,
+        CONSTRAINT pk_assistant PRIMARY KEY(id),
+        CONSTRAINT fk_assistant_users FOREIGN KEY(user_pilares) REFERENCES users(id)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+        CONSTRAINT fk_assistant_event FOREIGN KEY(event) REFERENCES events(id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+)ENGINE=InnoDb;
         /* CONSTRAINT fk_tableA_tableB FOREIGN KEY(field) REFERENCES tableB(field)
         ON DELETE SET ON UPDATE NO ACTION
         ATTACH DATABASE '' AS ;
